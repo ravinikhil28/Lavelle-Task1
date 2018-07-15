33aece250a4bc26b498718cf1d0a7dfc8605b025
ravinikhil28 <nikhil.ravi.nikhil@gmail.com>
2018-07-15 17:57:21 +0530

    exit
    exit confirmation message

--- index.php
+++ index.php
@@ -63,7 +63,16 @@ $(document).ready(function(){
 		if(exit==true){window.location = 'index.php?logout=true';}		
 	});
 });
-
+if(isset($_GET['logout'])){ 
+     
+    //Simple exit message
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
+    fclose($fp);
+     
+    session_destroy();
+    header("Location: index.php"); //Redirect the user
+}
 
 </script>
 <?php
