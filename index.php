1a6c71f2294fcc15b022cdf6dbf306ac3f45785f
ravinikhil28 <nikhil.ravi.nikhil@gmail.com>
2018-07-16 10:45:04 +0530

    load chat log
    load the file containing the chat log and alos auto scrolling of chat screen

--- index.php
+++ index.php
@@ -89,7 +89,23 @@ if(isset($_GET['logout'])){
 		$("#usermsg").attr("value", "");
 		return false;
 	});
-
+//Load the file containing the chat log
+	function loadLog(){		
+		var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
+		$.ajax({
+			url: "log.html",
+			cache: false,
+			success: function(html){		
+				$("#chatbox").html(html); //Insert chat log into the #chatbox div	
+				
+				//Auto-scroll			
+				var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
+				if(newscrollHeight > oldscrollHeight){
+					$("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+				}				
+		  	},
+		});
+	}
 </script>
 <?php
 }
